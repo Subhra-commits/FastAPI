1bd6212d54296e9f0f85be9a5903976aab001c2e
Subhra-commits <subhradeepmaji13@gmail.com>
2026-06-04 16:15:26 +0530

    chore: added validation into model

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 class Products(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    quantity: int
+    id: Annotated[int, Field(..., description="Product ID", examples={1})]
+    name: Annotated[str, Field(..., max_lenth = 30, description = "Product name")]
+    description: Annotated[str, Field(..., description = 'Product description')]
+    price: Annotated[float, Field(..., gt = 0, description = 'Product price')]
+    quantity: Annotated[int, Field(..., gt = 0, description = 'Product quantity')]
 
     
